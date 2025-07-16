943ce38541379e634a30ced5be78f4f338d7fdf0
vardannm <vardan.torosyan40@gmail.com>
2025-07-16 20:52:47 +0400

    removing unnecessary old items

--- src/Template.jsx
+++ src/Template.jsx
@@ -1,12 +1,7 @@
 import CanvasEditor from "./components/CanvasItem/CanvasEditor";
 import { useLocation } from "react-router-dom";
 import './Template.css'
-import coffeeMenu from "../../../Downloads/menu-design-1741177484759.json";
-import fastFoodCombo from "../../../Downloads/menu-design-1741177538337.json";
-const templateData = {
-  1: coffeeMenu, 
-  2: fastFoodCombo,
-};
+
 function Template() {
   const location = useLocation();
 
